972355756d4c2fac6afca896b9800c456a03b10b
hans8787 <oyisamfarhan@gmail.com>
2019-07-01 18:38:53 +0700

    mengubah submenu

--- application/views/menu/submenu.php
+++ application/views/menu/submenu.php
@@ -15,7 +15,7 @@
 
 				<?= $this->session->flashdata('message'); ?>
 
-		  		<a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newSubMenuModal">Add New Submenu</a>
+		  		<a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newSubMenuModal">Add New Submenu 1</a>
 
 		  		<table class="table table-hover">
 				  <thead>
